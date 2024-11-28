fe6c21d2d7b670d75d7fd19851a1434cf5c3d7c9
Ralph <ralph.sleigh@gmail.com>
2024-11-28 14:11:36 +0000

    Use specific domain

--- terraform/website.tf
+++ terraform/website.tf
@@ -2,6 +2,6 @@ module "hugosite" {
   source              = "github.com/fillup/terraform-hugo-s3-cloudfront"
   aliases             = ["webtest.ealingwoodcraft.org.uk"]
   bucket_name         = "webtest.ealingwoodcraft.org.uk"
-  cert_domain         = "ealingwoodcraft.org.uk"
+  cert_domain         = "webtest.ealingwoodcraft.org.uk"
   deployment_user_arn = var.deployment_user_arn
 }
